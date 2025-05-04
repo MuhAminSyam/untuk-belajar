65647c3382c05f3c89abeb938092d912b261de73
MuhAminSyam <aminsyammuh@gmail.com>
2025-05-05 06:35:14 +0700

    contoh 2

--- pembuatan contoh.cpp
+++ pembuatan contoh.cpp
@@ -7,31 +7,3 @@ class mobil
 public:
     string nama_mobil;
     string tipe_mobil;
-
-private:
-    int harga_mobil;
-
-public:
-    void inputData()
-    {
-        cout << "masukkan nama mobil:";
-        cin >> nama_mobil;
-        cout << "masukkan tipe mobil:";
-        cin >> tipe_mobil;
-        cout << "masukkan harga mobil:";
-        cin >> harga_mobil;
-    }
-    void output_data()
-    {
-        cout << " harga mobil =" << harga_mobil << endl;
-    }
-};
-
-int main()
-{
-    mobil nazar;
-    nazar.inputData();
-    cout << "nama mobil = " << nazar.nama_mobil << endl;
-    cout << "tipe mobil = " << nazar.tipe_mobil << endl;
-    nazar.output_data();
-}
